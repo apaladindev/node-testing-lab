c716cec00b4d9180c73f18d49c14b5f291d46f2d
Alejandro Paladines <apaladines4@gmail.com>
2025-04-24 00:02:08 -0500

    Unit test for BookService implementing Stubs

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,19 +1,34 @@
 const BookService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BookService', () => {
   let service;
   beforeEach(() => {
     service = new BookService();
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
     test('Should return a book list', async () => {
       // Arrange
       // Act
-      const books = await service.getBooks({}); // reading from DB
+      const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(3);
+      expect(books.length).toEqual(1);
     });
 
     test('Should return a book list', async () => {
