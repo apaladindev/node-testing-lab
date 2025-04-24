77b4cf9dfa581c5501a8e78742c848af6bcde4e8
Alejandro Paladines <apaladines4@gmail.com>
2025-04-24 00:24:01 -0500

    Unit test for BookService implementing Spies

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -7,12 +7,12 @@ const fakeBooks = [
   },
 ];
 
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => {},
-};
+const mockSpyGetAll = jest.fn();
 
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockSpyGetAll,
+  create: () => {},
+})));
 
 describe('Test for BookService', () => {
   let service;
@@ -24,20 +24,25 @@ describe('Test for BookService', () => {
   describe('Test for getBooks', () => {
     test('Should return a book list', async () => {
       // Arrange
+      mockSpyGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
       expect(books.length).toEqual(1);
+      expect(mockSpyGetAll).toHaveBeenCalled();
+      expect(mockSpyGetAll).toHaveBeenCalledWith('books', {});
+      expect(mockSpyGetAll).toHaveBeenCalledTimes(1);
     });
 
     test('Should return a book list', async () => {
       // Arrange
+      mockSpyGetAll.mockResolvedValue([{ _id: 1, name: 'Atomic Habits' }]);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books[0].name).toEqual('Harry Potter');
+      expect(books[0].name).toEqual('Atomic Habits');
     });
   });
 });
